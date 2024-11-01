4ee4e9b0423e2729f67f973c8f6a30c36693d716
liao07211270 <144579670+liao07211270@users.noreply.github.com>
2024-11-01 11:25:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+$(document).ready(function() {
     // 平滑滾動到指定區塊
     $("nav ul li a").on("click", function(event) {
         event.preventDefault(); // 防止默認的錨點行為
@@ -11,12 +12,12 @@ $(document).ready(function() {
     // 游標在圖片上停留超過一秒時顯示文字描述
     let timer; // 初始化計時器
     $(".menu-img").on("mouseenter", function() {
-        const description = $(this).siblings(".description"); // 獲取與當前圖片同層的描述文字
+        const description = $(this).siblings(".menu-details").find(".description"); // 獲取描述文字
         timer = setTimeout(function() {
             description.fadeIn(); // 如果游標保持在圖片上超過一秒，則顯示文字描述
         }, 1000); // 1 秒的延遲
     }).on("mouseleave", function() {
         clearTimeout(timer); // 當游標離開圖片時，清除計時器
-        $(this).siblings(".description").fadeOut(); // 隱藏文字描述
+        $(this).siblings(".menu-details").find(".description").fadeOut(); // 隱藏文字描述
     });
 });
